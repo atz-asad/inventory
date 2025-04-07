7948014da598ca5582c23b401dded5fae37f3d05
asad <ashad731386@gmail.com>
2025-04-08 00:26:49 +0600

    hi

---
+++ database/migrations/2025_04_07_173707_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Create the users table
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('email', 50)->unique();
+            $table->string('mobile', 20)->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('otp', 10);
+            $table->string('remember_token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
